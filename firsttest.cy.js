8e85fade2b1d31c72e0305c0db22633e1d5b9ebc
iamhari07 <mandalapuhari45@gmail.com>
2024-12-19 18:40:40 +0530

    Add files via upload

---
+++ firsttest.cy.js
@@ -0,0 +1,15 @@
+describe('My First test', () => {
+  it('test1', () => {
+    
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM')
+
+  })
+
+  it('test2', () => {
+    
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    cy.title().should('eq','OrangeHRM123')
+
+  })
+})
